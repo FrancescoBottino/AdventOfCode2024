d82afc80b252e8d2d889732129113cb89bdebe30
Francesco Bottino <francesco.bottino96@gmail.com>
2024-12-05 11:29:01 +0100

    Added Day01

--- src/Utils.kt
+++ src/Utils.kt
@@ -2,11 +2,12 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.time.measureTimedValue
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+fun readInput(name: String) = Path("src/$name.txt").readText().trim()
 
 /**
  * Converts string to md5 hash.
@@ -19,3 +20,12 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun <T> printTimedResult(expectedValue: T? = null, operation: () -> T) {
+    val result = measureTimedValue(operation)
+
+    println("time taken: ${result.duration}, result: ${result.value}")
+    if(expectedValue != null) {
+        check(expectedValue == result.value)
+    }
+}
