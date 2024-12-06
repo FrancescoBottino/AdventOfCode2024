9d88f3579eb68827c8350c2b42a3f1c2812d9152
Francesco Bottino <francesco.bottino96@gmail.com>
2024-12-06 08:50:31 +0100

    Refactor

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,131 +1,134 @@
-fun main() {
-    val testInput = readInput("Day04_test")
-    val input = readInput("Day04")
+object Day04 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val testInput = readInput("Day04_test")
+        val input = readInput("Day04")
 
-    println("Part 1 test")
-    printTimedResult(18) {
-        part1(testInput)
+        println("Part 1 test")
+        printTimedResult(18) {
+            part1(testInput)
+        }
+
+        println("Part 1")
+        printTimedResult(2390) {
+            part1(input)
+        }
+
+        println("Part 2 test")
+        printTimedResult(9) {
+            part2(testInput)
+        }
+
+        println("Part 2")
+        printTimedResult(1809) {
+            part2(input)
+        }
     }
 
-    println("Part 1")
-    printTimedResult(2390) {
-        part1(input)
+    private fun parseInput(input: String): List<List<Char>> {
+        return input.lines().map { it.toList() }
     }
 
-    println("Part 2 test")
-    printTimedResult(9) {
-        part2(testInput)
+    private data class Position(val rowIndex: Int, val colIndex: Int)
+
+    private sealed class Direction(val rowOffset: Int, val colOffset: Int) {
+        data object UP_LEFT: Direction(-1, -1)
+        data object UP: Direction(-1, 0)
+        data object UP_RIGHT: Direction(-1, +1)
+        data object RIGHT: Direction(0, +1)
+        data object DOWN_RIGHT: Direction(+1, +1)
+        data object DOWN: Direction(+1, 0)
+        data object DOWN_LEFT: Direction(+1, -1)
+        data object LEFT: Direction(0, -1)
+
+        companion object {
+            val all = setOf(UP_LEFT, UP, UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT)
+        }
     }
 
-    println("Part 2")
-    printTimedResult(1809) {
-        part2(input)
+    private operator fun Position.plus(direction: Direction): Position {
+        return Position(rowIndex + direction.rowOffset, colIndex + direction.colOffset)
     }
-}
 
-private fun parseInput(input: String): List<List<Char>> {
-    return input.lines().map { it.toList() }
-}
-
-private data class Position(val rowIndex: Int, val colIndex: Int)
-
-sealed class Direction(val rowOffset: Int, val colOffset: Int) {
-    data object UP_LEFT: Direction(-1, -1)
-    data object UP: Direction(-1, 0)
-    data object UP_RIGHT: Direction(-1, +1)
-    data object RIGHT: Direction(0, +1)
-    data object DOWN_RIGHT: Direction(+1, +1)
-    data object DOWN: Direction(+1, 0)
-    data object DOWN_LEFT: Direction(+1, -1)
-    data object LEFT: Direction(0, -1)
-
-    companion object {
-        val all = setOf(UP_LEFT, UP, UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT)
+    private fun Position.isValid(grid: List<List<*>>): Boolean {
+        return rowIndex in grid.indices && colIndex in grid.first().indices
     }
-}
 
-private operator fun Position.plus(direction: Direction): Position {
-    return Position(rowIndex + direction.rowOffset, colIndex + direction.colOffset)
-}
+    private operator fun List<List<Char>>.get(position: Position): Char {
+        return this[position.rowIndex][position.colIndex]
+    }
 
-private fun Position.isValid(grid: List<List<*>>): Boolean {
-    return rowIndex in grid.indices && colIndex in grid.first().indices
-}
+    private fun isValidWord(grid: List<List<Char>>, position: Position, direction: Direction): Boolean {
+        val wordToFind = "XMAS"
 
-private operator fun List<List<Char>>.get(position: Position): Char {
-    return this[position.rowIndex][position.colIndex]
-}
+        var currentPosition = position
 
-private fun isValidWord(grid: List<List<Char>>, position: Position, direction: Direction): Boolean {
-    val wordToFind = "XMAS"
+        if(!currentPosition.isValid(grid)) return false
 
-    var currentPosition = position
-
-    if(!currentPosition.isValid(grid)) return false
-
-    wordToFind.forEachIndexed { charIndex, char ->
-        if(grid[currentPosition] == char) {
-            if(charIndex != wordToFind.lastIndex) {
-                currentPosition += direction
-                if(!currentPosition.isValid(grid)) return false
+        wordToFind.forEachIndexed { charIndex, char ->
+            if(grid[currentPosition] == char) {
+                if(charIndex != wordToFind.lastIndex) {
+                    currentPosition += direction
+                    if(!currentPosition.isValid(grid)) return false
+                }
+            } else {
+                return false
             }
-        } else {
-            return false
         }
+
+        return true
     }
 
-    return true
-}
+    private fun isValidCross(grid: List<List<Char>>, position: Position): Boolean {
+        val crossPoints = listOf(
+            Direction.UP_LEFT to Direction.DOWN_RIGHT,
+            Direction.UP_RIGHT to Direction.DOWN_LEFT,
+        )
 
-private fun isValidCross(grid: List<List<Char>>, position: Position): Boolean {
-    val crossPoints = listOf(
-        Direction.UP_LEFT to Direction.DOWN_RIGHT,
-        Direction.UP_RIGHT to Direction.DOWN_LEFT,
-    )
+        return crossPoints
+            .map { (a, b) -> (position + a) to (position + b) }
+            .all { (a, b) ->
+                a.isValid(grid) &&
+                        b.isValid(grid) &&
+                        ((grid[a] == 'M' && grid[b] == 'S') || grid[b] == 'M' && grid[a] == 'S')
+            }
+    }
 
-    return crossPoints
-        .map { (a, b) -> (position + a) to (position + b) }
-        .all { (a, b) ->
-            a.isValid(grid) &&
-            b.isValid(grid) &&
-            ((grid[a] == 'M' && grid[b] == 'S') || grid[b] == 'M' && grid[a] == 'S')
-        }
-}
+    private fun part1(input: String): Int {
+        val grid = parseInput(input)
 
-private fun part1(input: String): Int {
-    val grid = parseInput(input)
+        var count = 0
 
-    var count = 0
-
-    grid.forEachIndexed { rowIndex, row ->
-        row.forEachIndexed { colIndex, char ->
-            val position = Position(rowIndex, colIndex)
-            if(char == 'X') {
-                count += Direction.all.count { direction ->
-                    isValidWord(grid, position, direction)
+        grid.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { colIndex, char ->
+                val position = Position(rowIndex, colIndex)
+                if(char == 'X') {
+                    count += Direction.all.count { direction ->
+                        isValidWord(grid, position, direction)
+                    }
                 }
             }
         }
+
+        return count
     }
 
-    return count
-}
+    private fun part2(input: String): Int {
+        val grid = parseInput(input)
 
-private fun part2(input: String): Int {
-    val grid = parseInput(input)
+        var count = 0
 
-    var count = 0
-
-    grid.forEachIndexed { rowIndex, row ->
-        row.forEachIndexed { colIndex, char ->
-            val position = Position(rowIndex, colIndex)
-            if(char == 'A') {
-                if(isValidCross(grid, position)) {
-                    count ++
+        grid.forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { colIndex, char ->
+                val position = Position(rowIndex, colIndex)
+                if(char == 'A') {
+                    if(isValidCross(grid, position)) {
+                        count ++
+                    }
                 }
             }
         }
-    }
 
-    return count
+        return count
+    }
 }
--- src/Day05.kt
+++ src/Day05.kt
@@ -1,117 +1,120 @@
 import java.util.*
 
-fun main() {
-    val testInput = readInput("Day05_test")
-    val input = readInput("Day05")
+object Day05 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val testInput = readInput("Day05_test")
+        val input = readInput("Day05")
 
-    println("Part 1 test")
-    printTimedResult(143) {
-        part1(testInput)
-    }
-
-    println("Part 1")
-    printTimedResult(5964) {
-        part1(input)
-    }
-
-    println("Part 2 test")
-    printTimedResult(123) {
-        part2(testInput)
-    }
-
-    println("Part 2")
-    printTimedResult(4719) {
-        part2(input)
-    }
-}
-
-private fun parseInput(input: String): SafetyManual {
-    return input.split("\n\n", limit = 2).let { sections ->
-        SafetyManual(
-            sections[0].split("\n").map { ruleString ->
-                ruleString.split("|", limit = 2)
-                    .let { PageOrderingRule(it[0].toInt(), it[1].toInt()) }
-            },
-            sections[1].split("\n").map { updateString ->
-                updateString.split(",")
-                    .map { it.toInt() }
-                    .let { Update(it) }
-            },
-        )
-    }
-}
-
-private data class SafetyManual(
-    val rules: List<PageOrderingRule>,
-    val updates: List<Update>
-) {
-    val trailingMap = rules.groupBy({ it.leading }, { it.trailing }).mapValues { it.value.toSet() }
-    val leadingMap = rules.groupBy({ it.trailing }, { it.leading }).mapValues { it.value.toSet() }
-}
-
-private data class PageOrderingRule(
-    val leading: Int,
-    val trailing: Int,
-)
-
-private data class Update(
-    val pages: List<Int>
-)
-
-private fun <T> List<T>.swapAt(a: Int, b: Int): List<T> {
-    return toMutableList().also { Collections.swap(it, a, b) }.toList()
-}
-
-private fun List<Int>.getIncorrectPair(trailingMap: Map<Int, Set<Int>>, leadingMap: Map<Int, Set<Int>>): Pair<IndexedValue<Int>, IndexedValue<Int>>? {
-    val indexedPages = withIndex().toList()
-
-    indexedPages.forEach { indexedPage ->
-        val (index, page) = indexedPage
-
-        val allPossibleTrails = trailingMap[page]
-        val allPossibleLeads = leadingMap[page]
-
-        val leadingPages = indexedPages.subList(0, index)
-        val trailingPages = indexedPages.subList(index + 1, size)
-
-        //Only one check (on leading pages or trailing pages) is enough, but I'll leave both just for completion’s sake
-        val error = leadingPages.firstOrNull { leadingPage ->
-            (allPossibleLeads?.let { leadingPage.value !in it } ?: false) || (allPossibleTrails?.let { leadingPage.value in it } ?: false)
-        } ?: trailingPages.firstOrNull { trailingPage ->
-            (allPossibleLeads?.let { trailingPage.value in it } ?: false) || (allPossibleTrails?.let { trailingPage.value !in it } ?: false)
+        println("Part 1 test")
+        printTimedResult(143) {
+            part1(testInput)
         }
 
-        error?.let { return indexedPage to it }
+        println("Part 1")
+        printTimedResult(5964) {
+            part1(input)
+        }
+
+        println("Part 2 test")
+        printTimedResult(123) {
+            part2(testInput)
+        }
+
+        println("Part 2")
+        printTimedResult(4719) {
+            part2(input)
+        }
     }
 
-    return null
-}
-
-private fun List<Int>.getValueIfIncorrect(trailingMap: Map<Int, Set<Int>>, leadingMap: Map<Int, Set<Int>>): Int? {
-    var updatedPages = this
-
-    var incorrectPair = updatedPages.getIncorrectPair(trailingMap, leadingMap)
-    while(incorrectPair != null) {
-        updatedPages = updatedPages.swapAt(incorrectPair.first.index, incorrectPair.second.index)
-        incorrectPair = updatedPages.getIncorrectPair(trailingMap, leadingMap)
+    private fun parseInput(input: String): SafetyManual {
+        return input.split("\n\n", limit = 2).let { sections ->
+            SafetyManual(
+                sections[0].split("\n").map { ruleString ->
+                    ruleString.split("|", limit = 2)
+                        .let { PageOrderingRule(it[0].toInt(), it[1].toInt()) }
+                },
+                sections[1].split("\n").map { updateString ->
+                    updateString.split(",")
+                        .map { it.toInt() }
+                        .let { Update(it) }
+                },
+            )
+        }
     }
 
-    if(updatedPages == this)
+    private data class SafetyManual(
+        val rules: List<PageOrderingRule>,
+        val updates: List<Update>
+    ) {
+        val trailingMap = rules.groupBy({ it.leading }, { it.trailing }).mapValues { it.value.toSet() }
+        val leadingMap = rules.groupBy({ it.trailing }, { it.leading }).mapValues { it.value.toSet() }
+    }
+
+    private data class PageOrderingRule(
+        val leading: Int,
+        val trailing: Int,
+    )
+
+    private data class Update(
+        val pages: List<Int>
+    )
+
+    private fun <T> List<T>.swapAt(a: Int, b: Int): List<T> {
+        return toMutableList().also { Collections.swap(it, a, b) }.toList()
+    }
+
+    private fun List<Int>.getIncorrectPair(trailingMap: Map<Int, Set<Int>>, leadingMap: Map<Int, Set<Int>>): Pair<IndexedValue<Int>, IndexedValue<Int>>? {
+        val indexedPages = withIndex().toList()
+
+        indexedPages.forEach { indexedPage ->
+            val (index, page) = indexedPage
+
+            val allPossibleTrails = trailingMap[page]
+            val allPossibleLeads = leadingMap[page]
+
+            val leadingPages = indexedPages.subList(0, index)
+            val trailingPages = indexedPages.subList(index + 1, size)
+
+            //Only one check (on leading pages or trailing pages) is enough, but I'll leave both just for completion’s sake
+            val error = leadingPages.firstOrNull { leadingPage ->
+                (allPossibleLeads?.let { leadingPage.value !in it } ?: false) || (allPossibleTrails?.let { leadingPage.value in it } ?: false)
+            } ?: trailingPages.firstOrNull { trailingPage ->
+                (allPossibleLeads?.let { trailingPage.value in it } ?: false) || (allPossibleTrails?.let { trailingPage.value !in it } ?: false)
+            }
+
+            error?.let { return indexedPage to it }
+        }
+
         return null
-    else
-        return updatedPages[updatedPages.indices.last / 2]
-}
+    }
 
-private fun part1(input: String): Int {
-    val safetyManuel = parseInput(input)
-    return safetyManuel.updates
-        .filter { it.pages.getIncorrectPair(safetyManuel.trailingMap, safetyManuel.leadingMap) == null }
-        .sumOf { update -> update.pages[update.pages.indices.last / 2] }
-}
+    private fun List<Int>.getValueIfIncorrect(trailingMap: Map<Int, Set<Int>>, leadingMap: Map<Int, Set<Int>>): Int? {
+        var updatedPages = this
 
-private fun part2(input: String): Int {
-    val safetyManuel = parseInput(input)
-    return safetyManuel.updates
-        .mapNotNull { it.pages.getValueIfIncorrect(safetyManuel.trailingMap, safetyManuel.leadingMap) }
-        .sum()
+        var incorrectPair = updatedPages.getIncorrectPair(trailingMap, leadingMap)
+        while(incorrectPair != null) {
+            updatedPages = updatedPages.swapAt(incorrectPair.first.index, incorrectPair.second.index)
+            incorrectPair = updatedPages.getIncorrectPair(trailingMap, leadingMap)
+        }
+
+        return if(updatedPages == this)
+            null
+        else
+            updatedPages[updatedPages.indices.last / 2]
+    }
+
+    private fun part1(input: String): Int {
+        val safetyManuel = parseInput(input)
+        return safetyManuel.updates
+            .filter { it.pages.getIncorrectPair(safetyManuel.trailingMap, safetyManuel.leadingMap) == null }
+            .sumOf { update -> update.pages[update.pages.indices.last / 2] }
+    }
+
+    private fun part2(input: String): Int {
+        val safetyManuel = parseInput(input)
+        return safetyManuel.updates
+            .mapNotNull { it.pages.getValueIfIncorrect(safetyManuel.trailingMap, safetyManuel.leadingMap) }
+            .sum()
+    }
 }
